e377c0d1faf792279f7c2485cc3f9fdde183774e
szukacs <szukacs.gergely@gmail.com>
2021-07-03 13:27:51 +0200

    expense component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from './components/Expenses/Expenses'
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14, 0, 0, 0),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12, 0, 0, 0) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28, 0, 0, 0),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12, 0, 0, 0),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
